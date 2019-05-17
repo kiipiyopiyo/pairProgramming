2e3c70f95c8727fd393161314e974a79aa9c0c4d
g17903as <g17903as@gm.tsuda.ac.jp>
2019-05-17 17:23:17 +0900

    ドライバー　kiipiyopiyo ナビゲーター　nnao01294

---
+++ src/pairProgramming/IntToEng.java
@@ -0,0 +1,19 @@
+package pairProgramming;
+import java.util.Scanner;
+public class IntToEng {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        System.out.println(translateEng(input));
+	}
+	
+	static String translateEng(int n) {
+		String[] num = String.valueOf(n).split("");
+		
+		
+		return "";
+	}
+
+}
